c802eb37966f6e3fe936d8abf501b7baa10ff324
porag-m06 <porag.m06@gmail.com>
2024-01-28 01:45:16 +0600

    fixed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :post
-
-  after_save :post_comments_counter_updater
-
-  def post_comments_counter_updater
-    post.update(comments_counter: post.comments.count)
-    post.comments.count
-  end
+  belongs_to :post, counter_cache: :comments_counter
 end
